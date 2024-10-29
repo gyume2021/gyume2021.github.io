42f6373ff07d0fdcbd3d7a848fd03c12f57c3983
gyume2021 <103267909+gyume2021@users.noreply.github.com>
2024-10-29 01:46:53 +0100

    Add files via upload

---
+++ js/OSH-PHANTON.js
@@ -0,0 +1,7 @@
+function showImage(imageSrc) {
+    // Get the image element in the right column
+    const displayImage = document.getElementById('displayImage');
+
+    // Update the source of the display image
+    displayImage.src = imageSrc;
+}
